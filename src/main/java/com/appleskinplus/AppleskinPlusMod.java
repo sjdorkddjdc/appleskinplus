8a2515fd7f02e427be607959c96a8d6c5710e35c
sjdorkddjdc <fyfirik2@gmail.com>
2026-08-14 20:31:44 +0800

    fix yarn imports

--- src/main/java/com/appleskinplus/AppleskinPlusMod.java
+++ src/main/java/com/appleskinplus/AppleskinPlusMod.java
@@ -3,30 +3,25 @@ package com.appleskinplus;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.util.InputConstants;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class AppleskinPlusMod implements ClientModInitializer {
 
-    private static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(
-        ResourceLocation.fromNamespaceAndPath("appleskinplus", "category")
-    );
-
-    public static final KeyMapping TOGGLE_KEY = KeyBindingHelper.registerKeyBinding(
-        new KeyMapping(
+    public static final KeyBinding TOGGLE_KEY = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding(
             "key.appleskinplus.toggle",
-            InputConstants.Type.KEYSYM,
+            InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_F4,
-            CATEGORY
+            "category.appleskinplus"
         )
     );
 
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (TOGGLE_KEY.consumeClick()) {
+            while (TOGGLE_KEY.wasPressed()) {
                 FreecamController.toggle();
             }
         });
