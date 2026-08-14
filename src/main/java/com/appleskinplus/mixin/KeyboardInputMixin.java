b050c250d798df12cf61ac68711a30ee632409e7
sjdorkddjdc <fyfirik2@gmail.com>
2026-08-14 20:19:58 +0800

    Update KeyboardInputMixin.java

--- src/main/java/com/appleskinplus/mixin/KeyboardInputMixin.java
+++ src/main/java/com/appleskinplus/mixin/KeyboardInputMixin.java
@@ -1,8 +1,8 @@
 package com.appleskinplus.mixin;
 
 import com.appleskinplus.FreecamController;
-import net.minecraft.client.input.Input;
-import net.minecraft.client.input.KeyboardInput;
+import net.minecraft.client.player.Input;
+import net.minecraft.client.player.KeyboardInput;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,15 +11,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(KeyboardInput.class)
 public class KeyboardInputMixin {
 
-    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "tick", at = @At("RETURN"))
     private void appleskinplus$blockMovement(boolean slowDown, float slowDownFactor, CallbackInfo ci) {
         if (FreecamController.isActive()) {
             Input self = (Input) (Object) this;
-            self.movementForward = 0.0f;
-            self.movementSideways = 0.0f;
+            self.forwardImpulse = 0.0f;
+            self.leftImpulse = 0.0f;
             self.jumping = false;
-            self.sneaking = false;
-            ci.cancel();
+            self.shiftKeyDown = false;
         }
     }
 }
