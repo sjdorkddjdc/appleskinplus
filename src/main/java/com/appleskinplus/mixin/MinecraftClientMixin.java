c5dd930d7145db6c17c8ddbfb678fd0500899ca4
sjdorkddjdc <fyfirik2@gmail.com>
2026-08-14 21:36:12 +0800

    Update MinecraftClientMixin.java

--- src/main/java/com/appleskinplus/mixin/MinecraftClientMixin.java
+++ src/main/java/com/appleskinplus/mixin/MinecraftClientMixin.java
@@ -11,11 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
-    @Inject(method = "tick", at = @At("HEAD"))
-    private void appleskinplus$tick(CallbackInfo ci) {
-        FreecamController.tick();
-    }
-
     @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
     private void appleskinplus$blockAttack(CallbackInfoReturnable<Boolean> cir) {
         if (FreecamController.isActive()) {
