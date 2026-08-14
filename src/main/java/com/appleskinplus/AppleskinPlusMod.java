5fe1135272fb8c6076beb8e7d63970dd9215060c
sjdorkddjdc <fyfirik2@gmail.com>
2026-08-14 20:49:25 +0800

    fix keymapping mojang

--- src/main/java/com/appleskinplus/AppleskinPlusMod.java
+++ src/main/java/com/appleskinplus/AppleskinPlusMod.java
@@ -3,16 +3,16 @@ package com.appleskinplus;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.KeyMapping;
+import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 
 public class AppleskinPlusMod implements ClientModInitializer {
 
-    public static final KeyBinding TOGGLE_KEY = KeyBindingHelper.registerKeyBinding(
-        new KeyBinding(
+    public static final KeyMapping TOGGLE_KEY = KeyBindingHelper.registerKeyBinding(
+        new KeyMapping(
             "key.appleskinplus.toggle",
-            InputUtil.Type.KEYSYM,
+            InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_F4,
             "category.appleskinplus"
         )
@@ -21,7 +21,7 @@ public class AppleskinPlusMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (TOGGLE_KEY.wasPressed()) {
+            while (TOGGLE_KEY.consumeClick()) {
                 FreecamController.toggle();
             }
         });
