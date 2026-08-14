92b9b1cb9df24ca79663b76a857e780b5b4688ba
sjdorkddjdc <fyfirik2@gmail.com>
2026-08-14 21:03:00 +0800

    Update AppleskinPlusMod.java

--- src/main/java/com/appleskinplus/AppleskinPlusMod.java
+++ src/main/java/com/appleskinplus/AppleskinPlusMod.java
@@ -3,16 +3,20 @@ package com.appleskinplus;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
-import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
+/**
+ * Главный класс мода. Регистрирует горячую клавишу F4.
+ * Мод предназначен исключительно для кинематографической съёмки.
+ */
 public class AppleskinPlusMod implements ClientModInitializer {
 
-    public static final KeyMapping TOGGLE_KEY = KeyBindingHelper.registerKeyBinding(
-        new KeyMapping(
+    public static final KeyBinding TOGGLE_KEY = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding(
             "key.appleskinplus.toggle",
-            InputConstants.Type.KEYSYM,
+            InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_F4,
             "category.appleskinplus"
         )
@@ -21,7 +25,7 @@ public class AppleskinPlusMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (TOGGLE_KEY.consumeClick()) {
+            while (TOGGLE_KEY.wasPressed()) {
                 FreecamController.toggle();
             }
         });
