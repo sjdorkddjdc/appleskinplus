e4a6e8f66a453a1d666b4c66db5ba9127445efba
sjdorkddjdc <fyfirik2@gmail.com>
2026-09-06 15:39:16 +0800

    Update MinecraftClientMixin.java
    Client fix

--- src/main/java/com/appleskinplus/mixin/MinecraftClientMixin.java
+++ src/main/java/com/appleskinplus/mixin/MinecraftClientMixin.java
@@ -1,25 +1,37 @@
 package com.appleskinplus.mixin;
 
 import com.appleskinplus.FreecamController;
-import net.minecraft.client.MinecraftClient;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftClient.class)
+@Mixin(net.minecraft.client.MinecraftClient.class)
 public class MinecraftClientMixin {
 
-    @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
-    private void appleskinplus$blockAttack(CallbackInfoReturnable<Boolean> cir) {
+    @Inject(
+            method = "doAttack",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void appleskinplus$blockAttack(
+            CallbackInfoReturnable<Boolean> cir
+    ) {
         if (FreecamController.isActive()) {
             cir.setReturnValue(false);
         }
     }
 
-    @Inject(method = "doItemUse", at = @At("HEAD"), cancellable = true)
-    private void appleskinplus$blockItemUse(CallbackInfo ci) {
+    @Inject(
+            method = "doItemUse",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void appleskinplus$blockItemUse(
+            CallbackInfo ci
+    ) {
         if (FreecamController.isActive()) {
             ci.cancel();
         }
